fix: fbp computation for diffusion

uqct/models/diffusion.py
@@ -496,7 +496,8 @@ def denorm_image(
     type=bool,
     help="Whether to use a conditional diffusion model",
 )
-def main(dataset: DatasetName, sparse: bool, cond: bool):
+@click.option("--total-intensity", default=1e7, type=float, help="Total intensity")
+def main(dataset: DatasetName, sparse: bool, cond: bool, total_intensity):
     import lovely_tensors as lt
     import numpy as np
 
@@ -524,7 +525,6 @@ def main(dataset: DatasetName, sparse: bool, cond: bool):
         n_angles = 200
 
     angles = torch.from_numpy(np.linspace(0, 180, n_angles, endpoint=False)).to(device)
-    total_intensity = 1e5
     n_detectors_hr = gt.shape[-1]
     intensities = torch.tensor(total_intensity, device=device)
     if sparse:
@@ -542,7 +542,7 @@ def main(dataset: DatasetName, sparse: bool, cond: bool):
     diffusion = Diffusion(
         dataset,
         num_steps=100,
-        sgd_steps=10,
+        sgd_steps=0,
         lr=0.1,
         batch_size=16,
         cond=cond,

uqct/training/diffusion.py
@@ -18,6 +18,7 @@ from torch.utils.tensorboard.writer import SummaryWriter
 from tqdm.auto import tqdm
 
 from uqct.datasets.utils import get_dataset
+from uqct.debugging import plot_img
 from uqct.training.unet import N_ANGLES, norm_intensities, sample_fbp_sparse
 
 
@@ -121,7 +122,7 @@ def loss_fn_either(
     device: torch.device = torch.device("cpu"),
 ) -> torch.Tensor:
     if hasattr(unet, "unet"):
-        fbp, intensities, n_angles = sample_fbp_sparse(x_0, device)
+        fbp, intensities, n_angles = sample_fbp_sparse(x_0 * 0.5 + 0.5, device)
         x_0 = F.interpolate(x_0.to(unet.device), size=(128, 128), mode="area")
         loss = loss_fn_cond(x_0, fbp, intensities, n_angles, unet, noise_scheduler)  # type: ignore
     else: