fix: data set normalization

uqct/ct.py
@@ -3,6 +3,7 @@ from typing import Any, Callable, Literal
 
 import astra
 import numpy as np
+import numpy.typing as npt
 import torch
 
 
@@ -328,7 +329,7 @@ class Experiment:
         self.sparse = sparse
 
     def __str__(self) -> str:
-        return f"Experiment:\n  sparse: {self.sparse}\n  intensities: {self.intensities}\n  counts: {self.counts}\n  angles: {self.angles}"
+        return f"Experiment(sparse={self.sparse}, intensities={self.intensities}, counts={self.counts}, angles={self.angles})"
 
     def __repr__(self) -> str:
         return self.__str__()
@@ -576,7 +577,11 @@ def _apply_filter_batch(
     return sino_filt[0] if single else sino_filt
 
 
-def circular_mask(img_size: int, device=None, dtype=torch.float32) -> torch.Tensor:
+def circular_mask(
+    img_size: int,
+    device: torch.device | None = None,
+    dtype: torch.dtype = torch.float32,
+) -> torch.Tensor:
     device = device or torch.device("cpu")
     yy, xx = torch.meshgrid(
         torch.arange(img_size, device=device),
@@ -810,6 +815,81 @@ def fbp_2d(
 def apply_circular_mask(x: torch.Tensor) -> torch.Tensor:
     mask = circular_mask(x.shape[-1], device=x.device)
     return x * mask
+
+
+def prepare_inputs_from_experiment(
+    experiment: Experiment, schedule: torch.Tensor | None = None
+) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor | None]:
+    """
+    Parameters
+    ----------
+    experiment : Experiment
+        Experiment containing counts, intensities, and angles used to build model inputs.
+    schedule : torch.Tensor
+        Schedule of angles (sparse) or rounds (dense). It is expected to be an tensor of positive integers with shape (N,).
+
+    Returns
+    -------
+    torch.Tensor:
+        Tensor of shape `(..., 1, H, W)` or `(..., H, W)` containing filtered backprojection inputs in `[0, 1]`.
+    torch.Tensor:
+        Tensor of shape `(..., 1)` containing intensity per sample.
+    torch.Tensor | None:
+        Tensor of shape `(...,)` with integer class labels when the model is sparse, otherwise `None`.
+    """
+    if not experiment.sparse:
+        if schedule is not None:
+            raise NotImplementedError(
+                "Support for schedules is not yet supported for the dense setting."
+            )
+        counts = experiment.counts.cumsum(dim=-3)
+        intensities = experiment.intensities.cumsum(dim=-3)
+        sino = sinogram_from_counts(counts, intensities).clamp_min(0.0)
+        fbp_lr = fbp(sino, experiment.angles).clamp(0.0, 1.0)
+        n_angles = None
+        return (
+            fbp_lr.unsqueeze(-3),
+            intensities.sum(-2) * counts.shape[-1],
+            n_angles,
+        )
+
+    num_angles = len(experiment.angles)
+    fbps = list()
+    intensities = list()
+    if schedule is None:
+        schedule = torch.arange(1, num_angles + 1)
+    for i in schedule:
+        angles_i = experiment.angles[:i]
+        counts_i = experiment.counts[..., :i, :]
+        intensities_i = experiment.intensities[..., :i, :]
+        sino_i = sinogram_from_counts(counts_i, intensities_i)
+        fbp_i = fbp(sino_i, angles_i)
+        fbps.append(fbp_i)
+        intensities.append(intensities_i.sum((-2, -1)))
+    fbps = torch.stack(fbps, dim=-3).clamp(0, 1)
+    fbps.mul_(circular_mask(fbps.shape[-1]).to(fbps.device))
+    intensities = (
+        torch.stack(intensities, dim=-1).unsqueeze(-1) * experiment.counts.shape[-1]
+    )
+    class_labels = (
+        (schedule - 1)
+        .view(*((intensities.ndim - 2) * (1,)), len(schedule))
+        .expand(*intensities.shape[:-2], -1)
+    )
+    return fbps, intensities, class_labels
+
+
+def lr_from_experiment(experiment: Experiment) -> float:
+    """Computes and returns a best guess of the optimal learning rate.
+    The internal `lr` parameter gets set to this value."""
+    sparse2coef = {True: (-0.1655, 0.0188), False: {-0.0786, 0.01}}
+    beta_0, beta_1 = sparse2coef[experiment.sparse]
+    total_intensity = experiment.intensities.view(
+        math.prod(experiment.batch_dims), -1, 1
+    )[0].sum()
+    total_intensity *= experiment.counts.shape[-1]
+    lr = beta_1 * math.log(total_intensity) + beta_0
+    return lr
 
 
 if __name__ == "__main__":

uqct/datasets/nii_tomogram_dataset.py
@@ -3,6 +3,7 @@ import math
 import nibabel as nib
 import torch
 from PIL import Image
+import numpy as np
 
 from uqct.datasets.base_dataset import BaseImageDataset
 
@@ -14,7 +15,9 @@ class NIIWrapper:
         self.path = path
         self.im_size = im_size
 
-        self.all_images = nib.load(path).get_fdata()[file_range[0] : file_range[1]]  # type: ignore
+        self.all_images = np.asanyarray(nib.loadsave.load(path).dataobj)[
+            file_range[0] : file_range[1]
+        ]
         idx_to_slice_list = []
         global_index_to_local_list = []
         coordinates_list = []
@@ -52,7 +55,6 @@ class NIIWrapper:
 
 
 class NiiDataset(BaseImageDataset):
-
     def __init__(
         self,
         path,
@@ -65,21 +67,33 @@ class NiiDataset(BaseImageDataset):
     ):
         self.im_size = im_size
         self.images = NIIWrapper(path, im_size, file_range)
+
+        x = self.images.all_images.astype(np.float32)
+
+        if clip_range:
+            x = np.clip(x, clip_range[0], clip_range[1])
+            val_range = clip_range
+
+        if val_range:
+            x -= val_range[0]
+            x /= val_range[1] - val_range[0]
+        self.images.all_images = torch.from_numpy(x.astype(np.float32))
+
         super().__init__(
             rescale=rescale,
-            clip_range=clip_range,
-            val_range=val_range,
+            # clip_range=clip_range,
+            # val_range=val_range,
             rotation_angle=rotation_angle,
         )
 
     def __getitem__(self, idx):
-        image = torch.tensor(self.images[idx]).float()
+        image = self.images[idx].float()
 
         if len(image.shape) == 2:
             image = image.unsqueeze(0)
 
         image = self.transform(image)
-        return image
+        return image.clip(0, 1)
 
     def __len__(self):
         return len(self.images)

uqct/datasets/tiff_tomogram_dataset.py
@@ -62,7 +62,7 @@ class TIFFWrapper:
         return cropped_image
 
     def get_image(self, path):
-        if not path in self.images:
+        if path not in self.images:
             image = Image.open(os.path.join(self.path, path))
             self.images[path] = image
         return self.images[path]
@@ -72,7 +72,6 @@ class TIFFWrapper:
 
 
 class TIFFDataset(BaseImageDataset):
-
     def __init__(
         self,
         path,
@@ -92,13 +91,13 @@ class TIFFDataset(BaseImageDataset):
         )
 
     def __getitem__(self, idx):
-        image = torch.tensor(np.array(self.images[idx])).float()
+        image = torch.from_numpy(np.array(self.images[idx]).astype(np.float32))
 
         if len(image.shape) == 2:
             image = image.unsqueeze(0)
 
         image = self.transform(image)
-        return image
+        return image.clip(0, 1)
 
     def __len__(self):
         return len(self.images)

uqct/datasets/utils.py
@@ -30,7 +30,7 @@ KWARGS_LAMINO = {
     "path": DATA_DIR / "lamino_tiff",
     "rescale": 128,
     "im_size": 256,
-    "val_range": (0.0, 234.59),
+    "val_range": (0.0, 247.86),
     "rotation_angle": 30,
 }
 
@@ -45,9 +45,9 @@ KWARGS_COMPOSITE = {
     "path": DATA_DIR / "composite/SampleG-FBI22-Stitch-0-1-2.txm.nii",
     "rescale": 128,
     "im_size": 256,
-    "val_range": (0.0, 10646.63),
+    "val_range": (0.0, 42101.37),
     "file_range": [20, 360],
-    "clip_range": [3e4, 5e4],
+    # "clip_range": [3e4, 5e4],
     "rotation_angle": 30,
 }
 
@@ -79,7 +79,6 @@ def get_dataset(
 
     if dataset_type == "nii" and "clip_range" not in kwargs:
         kwargs["im_size"] = 512
-        kwargs["clip_range"] = [3e4, 5e4]
 
     dataset = dataset_class(**kwargs)
     torch.manual_seed(0)

uqct/models/diffusion.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 from typing import Callable, Literal
-import math
 
 import click
 import torch
@@ -11,9 +10,15 @@ from diffusers.utils.torch_utils import randn_tensor
 from torch import optim
 from tqdm.auto import tqdm
 
-from uqct.ct import Experiment, apply_circular_mask, circular_mask, nll
+from uqct.ct import (
+    Experiment,
+    apply_circular_mask,
+    circular_mask,
+    nll,
+    prepare_inputs_from_experiment,
+    lr_from_experiment,
+)
 from uqct.debugging import plot_img
-from uqct.models.unet import FBPUNet
 from uqct.training.diffusion import UNet2DModelAux
 from uqct.training.unet import N_ANGLES, norm_intensities
 
@@ -53,18 +58,6 @@ class Diffusion:
         # Guidance
         self.sgd_steps = sgd_steps
         self.lr = lr
-
-    def set_lr_from_experiment(self, experiment: Experiment) -> float:
-        """Computes and returns a best guess of the optimal learning rate.
-        The internal `lr` parameter gets set to this value."""
-        sparse2coef = {True: (-0.1655, 0.0188), False: {-0.0786, 0.01}}
-        beta_0, beta_1 = sparse2coef[experiment.sparse]
-        total_intensity = experiment.intensities.view(
-            math.prod(experiment.batch_dims), -1, 1
-        )[0].sum()
-        total_intensity *= experiment.counts.shape[-1]
-        self.lr = beta_1 * math.log(total_intensity) + beta_0
-        return self.lr
 
     def predict_noise_cond(
         self,
@@ -203,6 +196,7 @@ class Diffusion:
         pred_original_sample[..., ~mask] = -1.0
 
         if guidance_loss_fn is not None:
+            assert self.lr is not None
             pred_original_sample = guide(
                 pred_original_sample,
                 guidance_loss_fn,
@@ -271,7 +265,7 @@ class Diffusion:
             torch.Tensor: `(..., n_angles, replicates, 1, side_length, side_length)` (sparse) or `(..., n_rounds, replicates, 1, side_length, side_length)` (dense).
         """
         if self.lr is None:
-            self.set_lr_from_experiment(experiment)
+            self.lr = lr_from_experiment(experiment)
 
         side_length = experiment.counts.shape[-1]
         if experiment.sparse:
@@ -300,7 +294,7 @@ class Diffusion:
 
         it = tqdm(self.noise_scheduler.timesteps, disable=not self.verbose)
 
-        fbps, intensities, class_labels = FBPUNet._prepare_inputs_from_experiment(
+        fbps, intensities, class_labels = prepare_inputs_from_experiment(
             experiment, schedule
         )
         fbps = fbps.to(self.device)
@@ -457,8 +451,16 @@ def guide(
     sgd_steps: int = 50,
     lr: float = 0.1,
     optimizer: None | optim.Adam = None,
+    patience: None | int = None,
     verbose: bool = False,
 ) -> torch.Tensor:
+    """
+    Arguments:
+        x_t (`torch.Tensor`): Shape (..., H, W) images with pixel values in range [-1, 1]
+
+    Returns:
+        `torch.Tensor`: Guided shape (..., H, W) images with pixel values in range [-1, 1]
+    """
     circle_mask = torch.ones(*x_t.shape[-2:], device=x_t.device)
     radius = x_t.shape[-1] // 2
     y, x = torch.meshgrid(
@@ -480,6 +482,9 @@ def guide(
         with torch.no_grad():
             y.copy_(y_0)
     it = tqdm(range(sgd_steps), disable=not verbose)
+    lowest_loss = float("inf")
+    cur_patience = patience
+    best_y = None
     for _ in it:
         optimizer.zero_grad()
         yp = y * mask
@@ -498,9 +503,22 @@ def guide(
         with torch.no_grad():
             y.data[..., ~mask] = 0.0
 
-        it.set_postfix({"loss": f"{loss.item():.3f}"})
-    optimizer.zero_grad(set_to_none=True)
-    x_t_guided = norm_image(y).clip(-1, 1)
+        loss = loss.item()
+        if loss < lowest_loss:
+            lowest_loss = loss
+            best_y = y.detach().clone()
+            if cur_patience is not None:
+                cur_patience = patience
+
+        elif cur_patience is not None:
+            cur_patience -= 1
+
+        it.set_postfix({"loss": f"{loss:1.2e}", "lowest_loss": f"{lowest_loss:1.2e}"})
+
+        if cur_patience is not None and cur_patience <= 0:
+            break
+
+    x_t_guided = norm_image(best_y).clip(-1, 1)
     return x_t_guided
 
 

uqct/models/unet.py
@@ -4,6 +4,9 @@ import math
 from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 from typing import Literal, Self
+import pickle
+import tempfile
+
 
 import numpy as np
 import torch
@@ -14,9 +17,8 @@ from tqdm.auto import tqdm
 from uqct.ct import (
     Experiment,
     circular_mask,
-    fbp,
     sample_observations,
-    sinogram_from_counts,
+    prepare_inputs_from_experiment,
 )
 from uqct.datasets.utils import DatasetName, get_dataset
 from uqct.training.unet import (
@@ -70,6 +72,11 @@ class FBPUNetEnsemble:
         )
         return self
 
+    def to_disk(self) -> None:
+        self.pickle_path = tempfile.NamedTemporaryFile(delete=False).name
+        pickle.dump(self.unets, open(self.pickle_path, "wb"))
+        del self.unets
+
     def to(self, device: torch.device | str) -> FBPUNetEnsemble:
         target = torch.device(device)
         for unet in self.unets:
@@ -80,6 +87,7 @@ class FBPUNetEnsemble:
     def predict(
         self,
         experiment: Experiment,
+        schedule: torch.Tensor | None = None,
         *,
         out_device: torch.device | None = None,
         aggregate: Literal["none", "mean", "median"] = "mean",
@@ -104,14 +112,12 @@ class FBPUNetEnsemble:
         `torch.Tensor`
             Tensor of shape `(..., rounds, M, 1, H, W)` (dense) or `(..., M, 1, H, W)` (sparse) when `aggregate="none"` with `M` ensemble members, otherwise `(..., rounds, 1, H, W)` (dense) or `(..., 1, H, W)` (sparse) after aggregation, all values clipped to `[0, 1]`.
         """
-        fbp_lr, intensity_lr, class_labels = FBPUNet._prepare_inputs_from_experiment(
-            experiment
+        fbp_lr, intensity_lr, class_labels = prepare_inputs_from_experiment(
+            experiment, schedule
         )
         preds = []
         pbar = tqdm(self.unets) if verbose else self.unets
-        maybe_cuda = (
-            torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-        )
+        maybe_cuda = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         for unet in pbar:
             prev_device = unet.unet.device
             unet.to(maybe_cuda)
@@ -165,71 +171,9 @@ class FBPUNet:
         self.num_workers = num_workers
         self.out_device = out_device
 
-    def to(self, device: torch.device) -> "FBPUNet":
-        self.unet.to(device)  # type: ignore
+    def to(self, device: torch.device, dtype: torch.dtype = torch.float16) -> "FBPUNet":
+        self.unet.to(device, dtype=dtype)  # type: ignore
         return self
-
-    @staticmethod
-    def _prepare_inputs_from_experiment(
-        experiment: Experiment, schedule: torch.Tensor | None = None
-    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor | None]:
-        """
-        Parameters
-        ----------
-        experiment : Experiment
-            Experiment containing counts, intensities, and angles used to build model inputs.
-        schedule : torch.Tensor
-            Schedule of angles (sparse) or rounds (dense). It is expected to be an tensor of positive integers with shape (N,).
-
-        Returns
-        -------
-        torch.Tensor:
-            Tensor of shape `(..., 1, H, W)` or `(..., H, W)` containing filtered backprojection inputs in `[0, 1]`.
-        torch.Tensor:
-            Tensor of shape `(..., 1)` containing intensity per sample.
-        torch.Tensor | None:
-            Tensor of shape `(...,)` with integer class labels when the model is sparse, otherwise `None`.
-        """
-        if not experiment.sparse:
-            if schedule is not None:
-                raise NotImplementedError(
-                    "Support for schedules is not yet supported for the dense setting."
-                )
-            counts = experiment.counts.cumsum(dim=-3)
-            intensities = experiment.intensities.cumsum(dim=-3)
-            sino = sinogram_from_counts(counts, intensities).clamp_min(0.0)
-            fbp_lr = fbp(sino, experiment.angles).clamp(0.0, 1.0)
-            n_angles = None
-            return (
-                fbp_lr.unsqueeze(-3),
-                intensities.sum(-2) * counts.shape[-1],
-                n_angles,
-            )
-
-        num_angles = len(experiment.angles)
-        fbps = list()
-        intensities = list()
-        if schedule is None:
-            schedule = torch.arange(1, num_angles + 1)
-        for i in schedule:
-            angles_i = experiment.angles[:i]
-            counts_i = experiment.counts[..., :i, :]
-            intensities_i = experiment.intensities[..., :i, :]
-            sino_i = sinogram_from_counts(counts_i, intensities_i)
-            fbp_i = fbp(sino_i, angles_i)
-            fbps.append(fbp_i)
-            intensities.append(intensities_i.sum((-2, -1)))
-        fbps = torch.stack(fbps, dim=-3).clamp(0, 1)
-        fbps.mul_(circular_mask(fbps.shape[-1]).to(fbps.device))
-        intensities = (
-            torch.stack(intensities, dim=-1).unsqueeze(-1) * experiment.counts.shape[-1]
-        )
-        class_labels = (
-            (schedule - 1)
-            .view(*((intensities.ndim - 2) * (1,)), len(schedule))
-            .expand(*intensities.shape[:-2], -1)
-        )
-        return fbps, intensities, class_labels
 
     def _predict_from_tensors(
         self,
@@ -240,7 +184,7 @@ class FBPUNet:
         out_device: torch.device | None,
     ) -> torch.Tensor:
         """
-        Run inference on batched tensors produced by `_prepare_inputs_from_experiment`.
+        Run inference on batched tensors produced by `prepare_inputs_from_experiment`.
 
         Parameters
         ----------
@@ -326,6 +270,7 @@ class FBPUNet:
     def predict(
         self,
         experiment: Experiment,
+        schedule: torch.Tensor | None,
         *,
         out_device: torch.device | None = None,
     ) -> torch.Tensor:
@@ -336,6 +281,8 @@ class FBPUNet:
         ----------
         experiment : Experiment
             Experiment containing counts, intensities, and angles used to build model inputs.
+        schedule :  torch.Tensor | None
+            1D tensor containing the number of angles or rounds for which to generate predictions.
         out_device : torch.device | None
             Target device for the output tensor. Defaults to the device specified at init when `None`.
 
@@ -344,8 +291,8 @@ class FBPUNet:
         torch.Tensor
             Tensor of shape `(..., 1, H, W)` containing predictions in `[0, 1]`.
         """
-        fbp_lr, intensity_lr, class_labels = self._prepare_inputs_from_experiment(
-            experiment
+        fbp_lr, intensity_lr, class_labels = prepare_inputs_from_experiment(
+            experiment, schedule
         )
         return self._predict_from_tensors(
             fbp_lr,
@@ -356,15 +303,17 @@ class FBPUNet:
 
 
 def load_unet_ckpt(unet: UNet2DModel, ckpt_path: Path, verbose: bool = False) -> None:
-    ckpt = torch.load(ckpt_path, map_location="cpu")
-    sd = ckpt["unet"]
-    if any(k.startswith("_orig_mod.") for k in sd.keys()):
-        sd = {k.replace("_orig_mod.", "", 1): v for k, v in sd.items()}
-    unet.load_state_dict(sd, strict=True)
-    if verbose:
-        print(
-            f"Loaded checkpoint: epoch={ckpt.get('epoch', '?')}, val_loss={ckpt.get('val_loss', '?')}"
-        )
+    with ckpt_path.open("rb") as f:
+        ckpt = torch.load(f, map_location="cpu", weights_only=True)
+        sd = ckpt["unet"]
+        if any(k.startswith("_orig_mod.") for k in sd.keys()):
+            sd = {k.replace("_orig_mod.", "", 1): v for k, v in sd.items()}
+        unet.load_state_dict(sd, strict=True)
+        unet.to(dtype=torch.float16)  # type: ignore
+        if verbose:
+            print(
+                f"Loaded checkpoint: epoch={ckpt.get('epoch', '?')}, val_loss={ckpt.get('val_loss', '?')}"
+            )
 
 
 if __name__ == "__main__":
@@ -459,7 +408,7 @@ if __name__ == "__main__":
     gt_lr = torch.nn.functional.interpolate(gt, (128, 128), mode="area")
 
     model = FBPUNetEnsemble(dataset, model_label == "sparse", batch_size=16)
-    fbps, _, _ = model.unets[0]._prepare_inputs_from_experiment(exp)
+    fbps, _, _ = prepare_inputs_from_experiment(exp)
     preds = model.predict(exp, aggregate="mean", verbose=True)
     print(preds)
 
@@ -491,7 +440,7 @@ if __name__ == "__main__":
     # )
     # load_unet_ckpt(model.unet, ckpt_path)
     #
-    # fbps, _, _ = model._prepare_inputs_from_experiment(exp)
+    # fbps, _, _ = prepare_inputs_from_experiment(exp)
     # fbps = fbps.view(-1, 128, 128).to(device)
     # preds = model.predict(exp)
     # preds = preds.view(-1, 128, 128).to(device)